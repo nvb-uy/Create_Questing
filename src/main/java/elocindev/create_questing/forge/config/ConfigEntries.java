9b44b459afd30a344ee3c172777fc8786524ab58
nvb-uy <almhak@proton.me>
2024-01-16 12:07:20 -0300

    fix(chapter array being empty)

--- src/main/java/elocindev/create_questing/forge/config/ConfigEntries.java
+++ src/main/java/elocindev/create_questing/forge/config/ConfigEntries.java
@@ -1,6 +1,7 @@
 package elocindev.create_questing.forge.config;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import elocindev.necronomicon.api.config.v1.NecConfigAPI;
 import elocindev.necronomicon.config.NecConfig;
@@ -14,7 +15,12 @@ public class ConfigEntries {
     }
 
     public boolean enable_theme_only_in_create_chapters = true;
-    public ArrayList<String> create_chapter_names = new ArrayList<String>();
+    public ArrayList<String> create_chapter_names = new ArrayList<String>(
+        List.of(
+            "Create",
+            "Blueprint"
+        )
+    );
     public float blueprint_background_scale = 0.5F;
     public float blueprint_background_opacity = 0.9F;
     public int blueprint_background_overlay_red = 255;
